98ea8938e6e86d8c9b3dcd300985e5889d9aae5c
Aman Ulla <connectamanulla@gmail.com>
2026-01-15 11:06:18 +0530

    updated package version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="lopace",
-    version="0.1.0",
+    version="0.1.1",
     author="Aman Ulla",
     description="Lossless Optimized Prompt Accurate Compression Engine",
     long_description=long_description,
